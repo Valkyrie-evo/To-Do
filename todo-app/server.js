fe189ed8cbae2d3072fae46969f76b4499d23a4b
Valkyrie-evo <burnerisburned@gmail.com>
2025-10-16 05:13:27 +0000

    Basic Express Server

---
+++ todo-app/server.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
